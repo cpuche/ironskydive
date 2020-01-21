3c02e353ba2f9d281391ed10b113c7d427686a22
Ruben Fenoll <rfenollb@gmail.com>
2020-01-21 18:19:49 +0000

    Iniciando el ejercicio

---
+++ IronSkydive.js
@@ -0,0 +1,52 @@
+
+<nav>
+<header>
+<h1>“IronSkydive”</h1>
+<h2>“Let the trip begin”</h2>
+<aside>"The best experience"</aside>
+
+<div>
+<article>
+    <h3>"Hello" Welcome to IronSkyDive</h3>
+</article>
+<article>
+    <h4>"About Us" We like a lot programmin websites</h4>
+</article>
+<article>
+    <h5>"Wanna Join?" Join Out Fitness Program</h5>
+</article>
+</div>
+
+
+<div>
+<article>
+    <h6>1.Training</h6>
+    <p>We teach all the necessary things</p>
+</article>
+<article>
+    <h7>Get Ready</h7>
+    <p>You are already prepare</p>
+</article>
+<article>
+    <h8>Fly</h8>
+    <p>You are ready</p>
+    </article>
+<article>
+    <h9>Jump</h9>
+    <p>You have done the most complicated</p>
+</article>
+</div>
+
+
+<ul>
+
+<li>Day Structure</li>
+<li>Team</li>
+<li>Schedule</li>
+
+</ul>
+
+</header>
+
+
+</nav>
